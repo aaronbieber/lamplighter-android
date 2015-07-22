ea22b903fa851c67433d1de03c9b83c23a5f11a0
Aaron Bieber <aaron@aaronbieber.com>
2015-07-22 06:45:07 -0400

    Notification and icon.

--- app/src/main/java/com/aaronbieber/apps/lamplighter/receiver/WifiReceiver.java
+++ app/src/main/java/com/aaronbieber/apps/lamplighter/receiver/WifiReceiver.java
@@ -1,12 +1,17 @@
 package com.aaronbieber.apps.lamplighter.receiver;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.util.Log;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 
+import com.aaronbieber.apps.lamplighter.R;
+import com.aaronbieber.apps.lamplighter.activity.MainActivity;
 import com.aaronbieber.apps.lamplighter.service.NetworkCheckService;
 
 /**
@@ -20,12 +25,47 @@ public class WifiReceiver extends BroadcastReceiver {
         if (netInfo != null && netInfo.getType() == ConnectivityManager.TYPE_WIFI) {
             Log.d("WifiReceiver", "WiFi connection is now active.");
             if (NetworkCheckService.onHomeNetwork(context)) {
-                Log.d("WifiReceiver", "We are apparently home! Start heartbeat.");
-                NetworkCheckService.setHeartbeatAlarm(context);
+                Log.d("WifiReceiver", "We are apparently home!");
+
+                if(!NetworkCheckService.isHeartbeatAlarmSet(context)) {
+                    Log.d("WifiReceiver", "Heartbeat is not active, starting.");
+                    NetworkCheckService.setHeartbeatAlarm(context);
+                    sendNotification(context, 002, "Heartbeat Enabled", "Lamplighter heartbeat has been enabled.");
+                } else {
+                    Log.d("WifiReceiver", "Heartbeat is already active, resting.");
+                }
             }
         } else {
-            Log.d("WifiReceiver", "WiFi connection lost. Disable heartbeat.");
-            NetworkCheckService.stopHeartbeatAlarm(context);
+            Log.d("WifiReceiver", "WiFi connection lost.");
+            if (NetworkCheckService.isHeartbeatAlarmSet(context)) {
+                Log.d("WifiReceiver", "Heartbeat is active, deactivate it.");
+                NetworkCheckService.stopHeartbeatAlarm(context);
+                sendNotification(context, 001, "Heartbeat Disabled", "Lamplighter heartbeat has been disabled.");
+            } else {
+                Log.d("WifiReceiver", "Heartbeat is not active, resting.");
+            }
         }
+    }
+
+    private void sendNotification(Context context, int id, String title, String message) {
+        // Build intent for notification content
+        Intent viewIntent = new Intent(context, MainActivity.class);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(context, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(context)
+                        .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
+                        .setSmallIcon(R.drawable.ic_notification)
+                        .setContentTitle(title)
+                        .setContentText(message)
+                        .setContentIntent(viewPendingIntent);
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(context);
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(id, notificationBuilder.build());
     }
 }
