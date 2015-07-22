c3bd18ef75d475cc616a9c64e2a366c1a29ab355
Aaron Bieber <aaron@aaronbieber.com>
2015-07-21 22:27:38 -0400

    Seems to be working, mostly!

--- app/src/main/java/com/aaronbieber/apps/lamplighter/activity/MainActivity.java
+++ app/src/main/java/com/aaronbieber/apps/lamplighter/activity/MainActivity.java
@@ -1,20 +1,13 @@
 package com.aaronbieber.apps.lamplighter.activity;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.aaronbieber.apps.lamplighter.R;
 import com.aaronbieber.apps.lamplighter.service.NetworkCheckService;
@@ -30,10 +23,10 @@ public class MainActivity extends Activity {
 
         activity = this;
 
-        updateNetworkStatusLabel();
+        updateOnOffButton();
 
         // Events
-        Switch switchServiceToggle = (Switch) findViewById(R.id.switchServiceToggle);
+        ToggleButton switchServiceToggle = (ToggleButton) findViewById(R.id.toggleOnOff);
         switchServiceToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 boolean isAlarmSet = NetworkCheckService.isHeartbeatAlarmSet(activity.getApplicationContext());
@@ -56,7 +49,7 @@ public class MainActivity extends Activity {
                     }
                 }
 
-                updateNetworkStatusLabel();
+                updateOnOffButton();
             }
         });
 
@@ -64,30 +57,25 @@ public class MainActivity extends Activity {
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Update the textStatus label based on NetworkCheckService.isHeartbeatAlarmSet()
-                boolean isAlarmSet = NetworkCheckService.isHeartbeatAlarmSet(activity.getApplicationContext());
-                Log.i("Lamplighter", "Got value for isAlarmSet " + (isAlarmSet ? "Yes" : "No"));
-                TextView t = (TextView) findViewById(R.id.textStatus);
-                if (isAlarmSet) {
-                    t.setText("Running");
-                } else {
-                    t.setText("Stopped");
-                }
-
-                updateNetworkStatusLabel();
+                updateOnOffButton();
             }
         });
     }
 
-    private void updateNetworkStatusLabel() {
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        updateOnOffButton();
+    }
+
+    private void updateOnOffButton() {
         // Update the textStatus label based on NetworkCheckService.isHeartbeatAlarmSet()
         boolean isAlarmSet = NetworkCheckService.isHeartbeatAlarmSet(this.getApplicationContext());
-        TextView t = (TextView) findViewById(R.id.textStatus);
 
-        if (isAlarmSet) {
-            t.setText("Running");
-        } else {
-            t.setText("Stopped");
-        }
+        Log.i("Lamplighter", "Heartbeat service alarm is " + (isAlarmSet ? "on." : "off."));
+
+        ToggleButton t = (ToggleButton) findViewById(R.id.toggleOnOff);
+        t.setChecked(isAlarmSet);
     }
 }
